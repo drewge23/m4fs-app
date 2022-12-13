11ded1708f08e8a397e396e025804fdf4aa8ba52
drewge23 <screwnut152@gmail.com>
2022-12-13 13:24:46 +0200

    Login and Registration forms 1.0

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,26 +1,78 @@
 import {FC} from "react";
 import {NavLink} from "react-router-dom";
+import {Form, Field} from 'react-final-form'
+
+const sleep = (ms:any) => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async (values:any) => {
+    //DLA API goes here
+    await sleep(300)
+    window.alert(JSON.stringify(values))
+}
+
+const LoginForm: FC = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <Form
+                onSubmit={onSubmit}
+                initialValues={{}}
+                render={({handleSubmit, form, submitting, pristine, values}) => (
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Email</label>
+                            <Field
+                                name="email"
+                                component="input"
+                                type="email"
+                                placeholder="email@gmail.com"
+                            />
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <Field
+                                name="password"
+                                component="input"
+                                type="password"
+                                placeholder=""
+                            />
+                        </div>
+                        <div>
+                            <label>remember me</label>
+                            <Field name="remember" component="input" type="checkbox"/>
+                        </div>
+                        <div className="buttons">
+                            <button type="submit" disabled={submitting || pristine}>
+                                Submit
+                            </button>
+                            <button
+                                type="button"
+                                onClick={form.reset}
+                                disabled={submitting || pristine}
+                            >
+                                Reset
+                            </button>
+                        </div>
+                        <pre>{JSON.stringify(values)}</pre>
+                    </form>
+                )}
+            />
+        </div>
+    )
+}
 
 const Login: FC = () => {
     return (
-    <div>
-        <form action="#">
-            <label htmlFor="email">email</label>
-            <input name="email" type="email"/>
-            <br />
-            <label htmlFor="password">password</label>
-            <input name="password" type="password"/>
-            <br />
-            <button type="submit">submit</button>
-        </form>
-        <NavLink to={"/registration"}>
-            Sign up
-        </NavLink>
-        <br />
-        <NavLink to={"/"}>
-            Skip
-        </NavLink>
-    </div>
+        <div>
+            <LoginForm/>
+            <NavLink to={"/registration"}>
+                Sign up
+            </NavLink>
+            <br/>
+            <NavLink to={"/"}>
+                Skip
+            </NavLink>
+        </div>
     )
 }
 
--- src/Registration/Registration.tsx
+++ src/Registration/Registration.tsx
@@ -1,22 +1,114 @@
 import {FC} from "react";
 import {NavLink} from "react-router-dom";
+import {Form, Field} from "react-final-form";
+import './Registration.module.css';
+
+const onSubmit = (values: any) => alert(JSON.stringify(values));
+
+const RegistrationForm: FC = () => {
+    return (
+        <div className="formContainer">
+            <h1>Registration</h1>
+            <Form
+                onSubmit={onSubmit}
+                render={({handleSubmit, form, submitting, pristine, values}) => (
+                    <form onSubmit={handleSubmit}>
+                        <Field name="firstName">
+                            {({input, meta}) => (
+                                <div>
+                                    <label>First name</label>
+                                    <input {...input} type="text" placeholder="John"/>
+                                </div>
+                            )}
+                        </Field>
+                        <div>
+                            <label>Last name</label>
+                            <Field
+                                name="lastName"
+                                component="input"
+                                type="text"
+                                placeholder="Doe"
+                            />
+                        </div>
+                        <div>
+                            <label>User name</label>
+                            <Field
+                                name="userName"
+                                component="input"
+                                type="text"
+                                placeholder="Coolguy_2006😎"
+                            />
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <Field
+                                name="email"
+                                component="input"
+                                type="email"
+                                placeholder="johndoe@gmail.com"
+                            />
+                        </div>
+                        <div>
+                            <label>Phone number</label>
+                            <Field
+                                name="phoneNumber"
+                                component="input"
+                                type="tel"
+                                placeholder="+972 000-00-00"
+                            />
+                        </div>
+                        <div>
+                            <label>Date of birth</label>
+                            <Field
+                                name="dateOfBirth"
+                                component="input"
+                                type="date"
+                                placeholder=""
+                            />
+                        </div>
+                        <div>
+                            <label>Country</label>
+                            <Field
+                                name="country"
+                                component="input"
+                                type="text"
+                                placeholder=""
+                            />
+                        </div>
+                        <div>
+                            <label>Profile picture</label>
+                            <Field
+                                name="profilePic"
+                                component="input"
+                                type="file"
+                                placeholder=""
+                            />
+                        </div>
+                        <div>
+                            <button type="submit">
+                                Submit
+                            </button>
+                            <button type="reset">
+                                Reset
+                            </button>
+                        </div>
+                        <pre>{JSON.stringify(values)}</pre>
+                    </form>
+                )
+                }
+            />
+        </div>
+    )
+}
 
 const Registration: FC = () => {
     return (
-        <form action="#">
-            <label htmlFor="email">email</label>
-            <input name="email" type="email"/>
-            <label htmlFor="password">password</label>
-            <input name="password" type="password"/>
-            <label htmlFor="email">email</label>
-            <input name="email" type="email"/>
-            <label htmlFor="password">password</label>
-            <input name="password" type="password"/>
-            <button type="submit">submit</button>
+        <div>
+            <RegistrationForm/>
             <NavLink to={"/"}>
                 Skip
             </NavLink>
-        </form>
+        </div>
     )
 }
 
